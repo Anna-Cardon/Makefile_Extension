ad29f767ad927a9774998c27d1f8a7a9ccfe97f2
Anna-Cardon <annastacia.cardon@gmail.com>
2023-11-19 17:54:42 -0600

    New steps

--- src/fetching/makefileContentBuilder.ts
+++ src/fetching/makefileContentBuilder.ts
@@ -19,10 +19,11 @@ export async function makefileBuilder (filePath: string, files : any): Promise <
     }else if (pickFile) {
         var picked = pickFile;
     }
-
+    const objectFile = "main.o";
     fs.writeFileSync(
         filePath, 
-        functionName + ': '+ picked +"\n" + "\t gcc " + picked + " -o " + functionName + " \n", 
+        //functionName + ': '+ picked +"\n" + "\t gcc " + picked + " -o " + functionName + " \n",
+        functionName + ': '+ picked.replace(".cpp", ".o") +"\n" + "\t gcc " + picked.replace(".cpp",".o") + " -o " + functionName + " \n"+ picked.replace(".cpp",".o") + ': '+ picked +"\n" + "\t gcc " + picked + " -c " + " \n" + "clean: \n" + "\t rm -f *.o ",  
         {
         encoding: "utf8",
         flag: "a+",
@@ -32,5 +33,32 @@ export async function makefileBuilder (filePath: string, files : any): Promise <
 
     //Talks to the terminal and executes but need to edit the makefile content to be correct for no error, plus add error handling
     const terminal = vscode.window.createTerminal(`Ext Terminal Make`);
+    terminal.sendText("make clean");
     terminal.sendText("make");
+
 }
+
+//what if the student updates files and wants to run the make, add options to go forward if a makefile already exists
+//update my readme file
+// published the extension for distribution or email
+
+// functionName + ': '+ picked.string.replace(".cpp", ".o") +"\n" 
+//         + "\t gcc " + picked.string.replace(".cpp",".o") + " -o " + functionName + " \n"
+//         + objectFile + ': '+ picked +"\n" 
+//         + "\t gcc " + picked + " -c " + " \n" 
+//         + "clean : \n" + "\t rrm *.o ", 
+
+
+// test3: main.o
+// 	 gcc main.o -o test3 
+
+// main.o: main.cpp
+// 	gcc main.cpp -c
+
+// //update for the three step process: 1. create main.o (an object file) make main.o command run
+// // 2. create an executable file called test3 through main.object
+// // 3. run test3
+
+// //can also add the make clean command to clear object files
+// clean:
+// 	rrm *.o 
