6767d3be2761cd191885bb6a4f5999cd08284ce8
Anna-Cardon <annastacia.cardon@gmail.com>
2024-01-18 01:11:43 -0600

    made multi choice, updated makefile creator

--- src/fetching/makefileContentBuilder.ts
+++ src/fetching/makefileContentBuilder.ts
@@ -2,28 +2,44 @@ import * as vscode from 'vscode';
 const fs = require('fs');
 import path = require('path');
 
+var targetExecutable;
+const objectFileCreateArray : string[] = [];
+
 //TODO: Think about the vscode.uri instead of a string
-export async function makefileBuilder (filePath: string, files : any): Promise <any>{
+export async function makefileBuilder (filePath: string, files : any): Promise <any>{ //note add .h files as options to pull up
 
     const functionName = await vscode.window.showInputBox({prompt: "Input the function name"});
     //If the user escapes or enters an empty string in the inputbox, then exit the function
     if(functionName === undefined || functionName === ''){
         return;
     }
-    const pickFile = await vscode.window.showQuickPick( [{label: "Pick a file for the MakeFile"}, ...files], {
+    const pickFiles = await vscode.window.showQuickPick(files, {
         ignoreFocusOut : true,
-        placeHolder : "Select a file.",
+        placeHolder : "Select a file(s).",
+        canPickMany: true, //enable multiple selection
     });
-    if (pickFile === undefined){ //user escapes the selection
+    if (!pickFiles || pickFiles.length === 0){ //user escapes the selection
         return;
-    }else if (pickFile) {
-        var picked = pickFile;
+    }else  {
+        // Process each selected file and concatenate the results into a single string/generates the string of source files : file1.o file2.o file3.o
+        targetExecutable = pickFiles.map(pickedFile => pickedFile.replace(".cpp", ".o")).join(' ');
+        console.log(targetExecutable);
+        
+        //Process the selected files into an object file creator dialoge
+        pickFiles.forEach(pickedFile => {
+            const outputFileName = pickedFile.replace(".cpp", ".o");
+            const objectFileCreate = outputFileName + ": " + pickedFile +"\n" + "\t g++ " + pickedFile + " -c " ;
+            objectFileCreateArray.push(objectFileCreate);
+            console.log(objectFileCreate);
+            // Now 'result' contains the formatted output for each picked file.
+            // You can use or store 'result' as needed in your code.
+        });
     }
     const objectFile = "main.o";
     fs.writeFileSync(
         filePath, 
         //functionName + ': '+ picked +"\n" + "\t gcc " + picked + " -o " + functionName + " \n",
-        functionName + ': '+ picked.replace(".cpp", ".o") +"\n" + "\t g++ " + picked.replace(".cpp",".o") + " -o " + functionName + " \n"+ picked.replace(".cpp",".o") + ': '+ picked +"\n" + "\t g++ " + picked + " -c " + " \n" + "clean: \n" + "\t rm -f *.o ",  
+        functionName + ': '+ targetExecutable +"\n" + "\t g++ " + targetExecutable + " -o " + functionName + " \n"+ objectFileCreateArray.join('\n') + " \n" + "clean: \n" + "\t rm -f *.o "+ functionName,  
         {
         encoding: "utf8",
         flag: "a+",
@@ -36,8 +52,22 @@ export async function makefileBuilder (filePath: string, files : any): Promise <
     terminal.sendText("make clean");
     terminal.sendText("make");
 
+    // Reset objectFileCreateArray to have no values
+    objectFileCreateArray.length = 0;
 }
+/*
+testingMake: main.o helper.o
+	g++ main.o helper.o -o testingMake
 
+main.o: main.cpp
+	g++ main.cpp -c
+
+helper.o: helper.cpp helper.h
+	g++ helper.cpp -c
+
+clean:
+	rm -f *.o testingMake
+*/
 //what if the student updates files and wants to run the make, add options to go forward if a makefile already exists
 //update my readme file
 // published the extension for distribution or email
